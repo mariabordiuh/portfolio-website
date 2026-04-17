36318764d62035fda80e949a182810bd7828ab4e
mariabordiuh <163565695+mariabordiuh@users.noreply.github.com>
2026-04-17 22:50:06 +0300

    Point app entry at stable shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import StableApp from './StableApp.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StableApp />
   </StrictMode>,
 );
